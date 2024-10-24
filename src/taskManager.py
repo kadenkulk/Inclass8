824384d0abc68edda92aec6ca6c88bd6bdca447e
Chris1234 <131827823+ChrisAldous@users.noreply.github.com>
2024-10-24 15:58:36 -0600

    Update taskManager.py

--- src/taskManager.py
+++ src/taskManager.py
@@ -3,6 +3,19 @@ class TaskManager:
     def __init__(self) -> None:
         self.login_info = None
         self.tasks = []
+
+    def add_task(self):
+        task = Task()
+    
+        task.name = input("Task Name: ")
+        task.description = input("Description: ")
+        task.due_date = input("Enter Due Date(MM-DD-YYYY): ")
+        task.completion_flag = False
+    
+        self.tasks.append(task)
+    
+        return None
+    
     def delete_task(self):
         task_to_remove = input("Please enter the exact name of the name you want to remove")
         for task in self.tasks:
