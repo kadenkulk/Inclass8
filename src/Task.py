a584669c8b80e5694e8a85dfff54fe62aa07b29b
Chris1234 <131827823+ChrisAldous@users.noreply.github.com>
2024-11-19 15:05:39 -0700

    Update Task.py
    Task is no longer a nested class

--- src/Task.py
+++ src/Task.py
@@ -1,25 +1,24 @@
 class Task:
-    class Task:
-        def __init__(self, name, description, due_date, priority=None):
+    def __init__(self, name, description, due_date, priority=None):
+        self.name = name
+        self.description = description
+        self.due_date = due_date
+        self.priority = priority
+        self.completed = False
+
+    def mark_as_complete(self):
+        self.completed = True
+
+    def edit_task(self, name=None, description=None, due_date=None):
+        if name is not None:
             self.name = name
+        if description is not None:
             self.description = description
+        if due_date is not None:
             self.due_date = due_date
-            self.priority = priority
-            self.completed = False
 
-        def mark_as_complete(self):
-            self.completed = True
+    def set_urgency(self, priority):
+        self.priority = priority
 
-        def edit_task(self, name=None, description=None, due_date=None):
-            if name is not None:
-                self.name = name
-            if description is not None:
-                self.description = description
-            if due_date is not None:
-                self.due_date = due_date
-
-        def set_urgency(self, priority):
-            self.priority = priority
-
-        def __repr__(self):
-            return f"{self.description} (Due: {self.due_date}) (priority: {self.priority})"
+    def __repr__(self):
+        return f"{self.description} (Due: {self.due_date}) (priority: {self.priority})"
